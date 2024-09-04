0d7cfcd10215c58eae8542e1fd9aada623a011e9
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-04 11:27:45 -0500

    JWT authentication logic

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,37 @@
+// auth.js
+// Contains JWT authentication logic, including the auth middleware for verifying tokens.
+
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Middleware function to verify the JWT token and attach the user data to the request
+  authMiddleware: function ({ req }) {
+    let token = req.headers.authorization || '';
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
+    if (token) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
+
+  // Signs a JWT token with user data
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
