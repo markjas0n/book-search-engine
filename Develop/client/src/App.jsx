2208359b49a49d8427e7a6b0c4dcb3af24bd5487
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-04 11:28:50 -0500

    Main entry point for the React app. Sets up the ApolloProvider to interact with the Apollo Server.

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Set up the HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Middleware to attach the JWT token to each request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
