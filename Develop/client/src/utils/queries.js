5666c65ba5a349d1703c63348f394c09ddf05bb3
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-04 11:29:37 -0500

    GraphQL queries for fetching data from the server.

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,23 @@
+
+// Defines GraphQL queries for fetching data from the server.
+
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
